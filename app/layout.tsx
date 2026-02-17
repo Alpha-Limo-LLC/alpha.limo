7aeefce4818a87385381fd3def48b3ba7e24ef09
bryanhohs <bryan@hohs.us>
2026-02-17 17:05:44 -0600

    new manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans, Noto_Sans_Mono } from "next/font/google";
+import Icons from "./icons";
 import "./styles/style.css";
 import "./globals.css";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${notoSans.variable} ${notoMono.variable} antialiased`}
       >
+        <Icons />
         {children}
       </body>
     </html>
