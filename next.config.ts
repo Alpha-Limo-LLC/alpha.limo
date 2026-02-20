c1d20f479706af9ce5065acf9a959fb15c1350e9
bryanhohs <bryan@hohs.us>
2026-02-20 17:26:26 -0600

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
+  cacheComponents: false,
   compress: true,
   reactStrictMode: false,
 };
 
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-    ]
-  },
+  crossOrigin: 'anonymous',
 }
 
 export default nextConfig;
