9f02c8293b93ffd651a5faad24d6215209b6d0db
bryanhohs <bryan@hohs.us>
2026-02-21 16:51:36 -0600

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,12 @@ export default function Alpha_Init() {
           priority
         ></Image>
         <div className="flex flex-col items-center gap-6 text-center p-5">
-          <h1 className="text-center text-5xl font-extrabold mt-5 text-black">Alpha Limo</h1>
+          <h1 className="text-center text-5xl font-extrabold mt-5 text-black">
+            Alpha Limo
+          </h1>
         </div>
         <div>
-          <Fade in={true} timeout={500}>
+          <Fade in={true} timeout={1000}>
             <h3 className="text-center text-md mt-5 text-black">
               {slogan}
             </h3>
