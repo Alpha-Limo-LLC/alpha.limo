e042e27e4bfb11531b4900e74a309b9dd07f33f7
bryanhohs <bryan@hohs.us>
2026-02-21 20:33:50 -0600

    new config

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,11 @@ export default function Alpha() {
   const ALPHA_FULL = config.site_name_full;
   const ALPHA_SHORT = config.site_name_short;
   const ALPHA_YEAR = config.site_year;
+  const ALPHA_URL = config.site_url;
   const SLOGAN = Slogan();
   return (
     <div className="flex justify-center align-center items-center w-full h-full bg-white">
-      <main className="w-full h-full flex flex-col justify-center items-center my-50">
+      <main className="flex flex-col justify-center items-center my-50">
         <Image
           src={Logo}
           alt={ALPHA_FULL}
@@ -24,20 +25,20 @@ export default function Alpha() {
           height={250}
           priority
         ></Image>
-        <div className="flex flex-col items-center gap-6 text-center p-5">
-          <h1 className="text-center text-5xl font-extrabold mt-5 text-black">
+        <div className="flex items-center gap-6 mt-10">
+          <h1 className="text-center text-5xl font-extrabold text-black">
             {ALPHA_SHORT}
           </h1>
         </div>
-        <div>
+        <div className="flex items-center mt-10">
           <Fade in={true} timeout={1000}>
-            <h3 className="text-center text-md mt-5 text-black">
+            <h3 className="text-center text-md text-black">
               {SLOGAN}
             </h3>
           </Fade>
         </div>
-        <div className="text-center static bottom-0 text-sm mt-5 p-5">
-          <Link className="text-normal no-underline hover:none visited:none text-black" href="https://github.com/Alpha-Limo-LLC/alpha.limo" target="_blank">
+        <div className="text-center static bottom-0 text-sm mt-10">
+          <Link className="text-normal no-underline hover:none visited:none text-black" href={ALPHA_URL} target="_blank">
             &copy; {ALPHA_YEAR} {ALPHA_FULL}
           </Link>
         </div>
