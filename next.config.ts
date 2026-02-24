238106c20dd98fe48f8268cd7d74df48198b4149
bryanhohs <bryan@hohs.us>
2026-02-23 19:54:00 -0600

    lint

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,6 @@ const nextConfig: NextConfig = {
 
 module.exports = {
   crossOrigin: 'anonymous',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     qualities: [100, 75, 50, 25],
   },
