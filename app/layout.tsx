99ab9ec11dd92b0488760eca50129a8580e5a06a
Bryan Hohs <bryan@hohs.us>
2026-03-24 16:34:55 +0000

    fixed font format

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <ClientProviders>{children}</ClientProviders>
         <SpeedInsights />
         <Analytics />
